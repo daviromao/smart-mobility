c6a4260e33088a8680d812877d0d52a652fea390
Davi Romão <davi.romao0210@gmail.com>
2023-07-07 12:35:22 -0300

    feat: add resource callback to simulate bus control

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
+import { resourceCallback } from "./controllers/bus-controller";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
 });
+
+app.post("/resource-webhook/:busUuid", resourceCallback);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
