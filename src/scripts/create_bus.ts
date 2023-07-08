359ae9ed4c2df7f9fa96c874f2dfbda095e3e4b6
Davi Romão <davi.romao0210@gmail.com>
2023-07-08 16:22:31 -0300

    refactor: put scripts into src and chage to typescript

--- src/scripts/create_bus.ts
+++ src/scripts/create_bus.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import * as fs from "fs";
 
+import { join } from "path";
+
 const prisma = new PrismaClient();
 
+const baseUrl = "http://10.10.10.1:3000";
+
 async function main() {
-  const rawdata = fs.readFileSync("./scripts/bus.json", "utf8");
+  const rawdata = fs.readFileSync(join(__dirname, "./bus.json"), "utf8");
   const resources = JSON.parse(rawdata);
 
   for (const resource of resources) {
@@ -17,6 +21,23 @@ async function main() {
     });
 
     const data = (await response.json()).data;
+
+    const subscription = await fetch(
+      "http://10.10.10.104:8000/adaptor/subscriptions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          subscription: {
+            uuid: data.uuid,
+            capabilities: data.capabilities,
+            url: `${baseUrl}/resource-webhook/${data.uuid}`,
+          },
+        }),
+      }
+    );
 
     console.log(
       await prisma.bus.create({
